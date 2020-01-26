118651d13384c36a499da04d5b9463870d26d33c
Tom Fjällström <tom.fjallstrom@gmail.com>
2020-01-26 17:31:47 +0100

    initial

---
+++ src/main/java/com/example/Backend2020/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.Backend2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller //Viktigt att komma ihåg
+@SpringBootApplication
+public class DemoApplication {
+	@RequestMapping("/")
+	@ResponseBody
+	String Home() {
+		
+		return "Hello World!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Backend2020/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
