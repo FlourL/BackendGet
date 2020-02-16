e9b516d883e1bf3fa395f212e9eb58a80c45db00
Tom Fjällström <tom.fjallstrom@gmail.com>
2020-02-16 13:39:22 +0100

    fixed clearUsers to not maintain id

--- src/main/java/com/example/Backend2020/Connectin.java
+++ src/main/java/com/example/Backend2020/Connectin.java
@@ -24,16 +24,7 @@ import java.util.List;
 						);
 				System.out.println("Connected to database");
 				conn.prepareStatement("CREATE DATABASE IF NOT EXISTS user_db;").executeUpdate(); //must write Schema apparently
-				conn.setCatalog("user_db");
-				System.out.println("USERDB REACHED");
-				conn.prepareStatement( //this whole statement would have to be remade for modification purposes, but yeah
-						"CREATE TABLE IF NOT EXISTS Users (" + 
-						"id Integer NOT NULL AUTO_INCREMENT," + 
-						"name VARCHAR(255)," + 
-						"proffesion VARCHAR(255)," + 
-						"CONSTRAINT user_id PRIMARY KEY (id)" + 
-						");").executeUpdate();
-				System.out.println("CREATED TABLE");
+				createTable();
 			}catch(SQLException e) {
 				System.out.println("Error: " + e.getMessage());
 			}
@@ -47,15 +38,7 @@ import java.util.List;
 				System.out.println("Connected to database");
 				//initDBAndTable();
 				conn.prepareStatement("CREATE SCHEMA IF NOT EXISTS user_db;").executeUpdate(); //must write Schema apparently
-				conn.setCatalog("user_db");
-				System.out.println("USERDB REACHED");
-				conn.prepareStatement( //this whole statement would have to be remade for modification purposes, but yeah
-						"CREATE TABLE IF NOT EXISTS Users (" + 
-						"id SERIAL NOT NULL PRIMARY KEY," + 
-						"name VARCHAR(255)," + 
-						"proffesion VARCHAR(255)" +
-						");").executeUpdate();
-				System.out.println("CREATED TABLE");
+				createTable();
 			}catch(SQLException e) {
 				System.out.println("Error: " + e.getMessage());
 			}
@@ -95,10 +78,28 @@ import java.util.List;
 		}
 		public synchronized void clearUsers() {
 			try {
-				conn.prepareStatement("TRUNCATE TABLE Users;").executeUpdate();
+				conn.prepareStatement("DROP TABLE Users;").executeUpdate();
+				createTable();
 			}catch(SQLException e) {
 				e.printStackTrace();
 			}
+		}
+		private void createTable() {
+			try {
+				conn.setCatalog("user_db");
+				System.out.println("USERDB REACHED");
+				conn.prepareStatement( //this whole statement would have to be remade for modification purposes, but yeah
+						"CREATE TABLE IF NOT EXISTS Users (" + 
+						"id Integer NOT NULL AUTO_INCREMENT," + 
+						"name VARCHAR(255)," + 
+						"proffesion VARCHAR(255)," + 
+						"CONSTRAINT user_id PRIMARY KEY (id)" + 
+						");").executeUpdate();
+				System.out.println("CREATED TABLE");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
 		}
 	}
 
