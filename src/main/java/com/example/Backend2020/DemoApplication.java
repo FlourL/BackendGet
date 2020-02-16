65ff825af4f8c4abf89aca02f828881ac40df1b3
Tom Fjällström <tom.fjallstrom@gmail.com>
2020-02-16 13:55:39 +0100

    removed irrelevant imports

--- src/main/java/com/example/Backend2020/DemoApplication.java
+++ src/main/java/com/example/Backend2020/DemoApplication.java
@@ -1,17 +1,7 @@
 package com.example.Backend2020;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class DemoApplication {
